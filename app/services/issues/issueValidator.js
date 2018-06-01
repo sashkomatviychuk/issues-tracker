2271b16087eaca87865f5823dcf0078d4eeb7b5a
Oleksandr Matviichuk <sashko1921@gmail.com>
2018-06-01 19:14:50 +0200

    Error messages

--- app/services/issues/issueValidator.js
+++ app/services/issues/issueValidator.js
@@ -2,10 +2,21 @@ const joi = require('joi');
 
 const schema = joi.object().keys(
     {
-        title: joi.string().max(255).required(),
-        description: joi.string().required(),
-        status: joi.string().required().valid(['open', 'pending', 'closed']),
-        priority: joi.string().required().valid(['low', 'normal', 'high']),
+        title: joi.string().max(255).required()
+            .label('Title')
+            .error(() => 'Title is required and max length 255 chars'),
+
+        description: joi.string().required()
+            .label('Description')
+            .error(() => 'Description is required'),
+
+        status: joi.string().required().valid(['open', 'pending', 'closed'])
+            .label('Status')
+            .error(() => 'Status can be only open, pending or closed'),
+
+        priority: joi.string().required().valid(['low', 'normal', 'high'])
+            .label('Priority')
+            .error(() => 'Priority can be only low, normal or high'),
     }
 );
 
